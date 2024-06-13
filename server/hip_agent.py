c841da47084df29308256ac241fe91ff6639355b
Midhun Dandamudi <midhunsezhi@gmail.com>
2024-06-13 15:16:56 +0530

    make prompt more clear with multiple shots

--- server/hip_agent.py
+++ server/hip_agent.py
@@ -28,32 +28,41 @@ class HIPAgent:
 
     def get_response(self, question, answer_choices):
         system_prompt = """
-         You're an assistant trying to answer multiple choice questions on Biology based on provided context.
-         You'll be given 4 options for each question and you have to select the correct one.
-         Only use the provided context to answer the question.
-         Your response should be the index of the correct option as a JSON object.
+         You are an assistant tasked with answering multiple choice questions on Biology using the provided context.
+         For each question, you will be given four options, and you must select the correct one based on the context. 
+         Evaluate all options thoroughly to determine the most accurate answer. If only one option is correct, select it. If multiple options are correct, select the most comprehensive option, if available (e.g., 'Both A and C').
+         Your response should be formatted as a JSON object indicating the index of the correct option and reason.
 
-         Example1:
+        Example1:
+            Context: The mitochondria is the powerhouse of the cell.
+            Question: What is the powerhouse of the cell?
+            Options: Nucleus, Mitochondria, Ribosome, Golgi apparatus
 
-            Context: The Los Angeles Dodgers won the World Series in 2020.
-            Question: Where was world series played?
-            Options: Los Angeles, New York, Miami, Chicago
-            
-            Your response: {"correct_index": 0}
-
+            Your response: {"correct_index": 1, "reason": "The mitochondria is known as the powerhouse of the cell."}
+          
         Example2:
             
-            Context: The Los Angeles Dodgers won the World Series in 2020.
-            Question: Who won the world series in 2020?
-            Options: New York Yankees, Los Angeles Dodgers, Miami Marlins, Chicago Cubs
-            
-            Your response: {"correct_index": 1}
+            Context: The five senses are sight, smell, taste, touch, and hearing.
+            Question: Which of the following are considered as traditional human senses?
+            Options: Sight, pain, Taste, Both A and C
+
+            Your response: {"correct_index": 3, "reason": "Sight and Taste are considered human senses."}
+
+         
+        Example3:
+            Context: types of blood cells include red blood cells, white blood cells, and platelets.
+            Question: Which of the following are types of blood cells?
+            Options: Red blood cells, Neurons, crap, Both A and C
+
+            Your response: {"correct_index": 0, reason: "only red blood cells are types of blood cells among the options provided."}
         """
+
         context = self.find_relevant_context(question)
 
         response = self.client.chat.completions.create(
             model="gpt-3.5-turbo-0125",
             response_format={ "type": "json_object" },
+            temperature=0,
             messages=[
                 {"role": "system", "content": system_prompt},
                 {"role": "user", "content": f"""
